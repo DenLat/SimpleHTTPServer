ac1f5ae033541fb0c290df6d5491f557ea7c1bc9
Denys Kostiuk <65499981+DenLat@users.noreply.github.com>
2024-09-26 21:06:54 +0200

    Initial commit, simple answer with Date at lockalhost

---
+++ src/main/java/org/app/Main.java
@@ -0,0 +1,26 @@
+package org.app;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Date;
+
+public class Main {
+
+    public static void main(String args[] ) throws IOException {
+
+        ServerSocket server = new ServerSocket(8080);
+        System.out.println("Listening for connection on port 8080 ....");
+        while (true) {
+            try (Socket socket = server.accept()) {
+                Date today = new Date();
+                String httpResponse = "HTTP/1.1 200 OK\r\n\r\n" + today;
+                socket.getOutputStream()
+                      .write(httpResponse.getBytes("UTF-8"));
+            }
+        }
+    }
+
+}
